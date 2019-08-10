105ce18a6e306ff3d3a9c0b42a957bbea239f576
Edward Lee <eblic71@gmail.com>
2019-08-10 15:31:14 -0400

    phase1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./App.css";
+import "bootstrap//dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return <h1>Hello from tech store</h1>;
+
+
+  };
+}
+
+
+export default App;
